3d763d495865c3132e87cf7e1b6a4f6d7be6f0d2
Héctor <hectoralvaez@gmail.com>
2022-08-25 10:05:21 +0200

    73. GifExpertApp - Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,14 @@
 export const GifExpertApp = () => {
     return(
         <>
+            {/* Título */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gifs */}
+                {/* Gif Item */}
+
         </>
     )
 }
